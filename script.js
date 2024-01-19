872b137540179d3926e4c2a0739b9c8bd7d44f66
Abhik <abhikgupta01@gmail.com>
2024-01-19 22:04:35 +0530

    Fourth commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Helllo");
+console.log("Hello There! Welcome to Console :)");
 let currentSong = new Audio();
 let songs;
 let currentFolder;
@@ -42,48 +42,23 @@ function secondsToMinutes(durationInSeconds) {
 
 async function fetchSongs(folder) {
     currentFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${currentFolder}/`);
-    let response = await a.text();
+    let x = await fetch(`http://127.0.0.1:5500/${currentFolder}/`);
+    let response = await x.text();
 
     let div = document.createElement("div");
     div.innerHTML = response;
-    const anchorTags = div.getElementsByTagName("a");
+    let anchorTags = div.getElementsByTagName("a");
 
-    let fetchedSongs = [];
+    let songs = [];
     for (let item of anchorTags) {
         if (item.href.endsWith(".mp3")) {
-            fetchedSongs.push(item.href.split(`/${currentFolder}/`)[1]);
+            songs.push(item.href.split(`/${currentFolder}/`)[1]);
         }
     }
-    return fetchedSongs;
-}
-
-
-// Function for playing the music
-function playMusic(audio, paused) {
-    currentSong.src = `/${currentFolder}/` + audio;
-    if(!paused) {
-        // Playing the music
-        currentSong.play();
-
-        // Changing play button to pause and vice versa
-        document.querySelector("#play").classList.remove("fa-play");
-        document.querySelector("#play").classList.add("fa-pause");
-    }
-    // Setting time and name of song
-    document.querySelector("#song-info > p").innerHTML = decodeURI(audio);
-    document.querySelector("#song-time > p").innerHTML = "00:00 / 00:00";
-}
-
-
-// Main Function
-async function main() {
-    // Fetch all songs
-    songs = await fetchSongs("Songs/Therapy");
-    playMusic(songs[0], true);
-
+    
     // Display songs in "Your Library" section
     let songsUnorderedList = document.querySelector("#songs-card > ul");
+    songsUnorderedList.innerHTML = "";
     for (let song of songs) {
         songsUnorderedList.innerHTML = songsUnorderedList.innerHTML + `<li>
             <div id="song-left">
@@ -105,6 +80,80 @@ async function main() {
             playMusic(e.querySelector("#song-center > h3").innerHTML, false);
         });
     });
+
+    return songs;
+}
+
+
+// Function for playing the music
+function playMusic(audio, paused) {
+    currentSong.src = `/${currentFolder}/` + audio;
+    if(!paused) {
+        // Playing the music
+        currentSong.play();
+
+        // Changing play button to pause and vice versa
+        document.querySelector("#play").classList.remove("fa-play");
+        document.querySelector("#play").classList.add("fa-pause");
+    }
+    // Setting time and name of song
+    document.querySelector("#song-info > p").innerHTML = decodeURI(audio);
+    document.querySelector("#song-time > p").innerHTML = "00:00 / 00:00";
+}
+
+
+// Funtion for fetching albums dynamically
+async function fetchAlbums() {
+    let y = await fetch(`http://127.0.0.1:5500/Songs/`);
+    let response = await y.text();
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let anchorTags = div.getElementsByTagName("a");
+
+    const cardContainer = document.querySelector("#cards");
+
+    // Iterating iver the collection and displaying Cards
+    let array = Array.from(anchorTags);
+    for(let i=0; i<array.length; i++){
+        const e = array[i];
+        if(e.href.includes("/Songs/")){
+            let folder = e.href.split("/").slice(-1)[0];
+            let z = await fetch(`http://127.0.0.1:5500/Songs/${folder}/info.json`);
+            let response = await z.json();
+            
+            cardContainer.innerHTML = cardContainer.innerHTML + `
+                <div data-folder="${folder}" class="card">
+                    <div class="card-image">
+                        <img src="/Songs/${folder}/cover.jpg" alt="Playlist">
+                        <div class="playCircle"><i class="fa-solid fa-play"></i></div>
+                    </div>
+                    <div class="card-data">
+                        <h2>${response.title}</h2>
+                        <p>${response.description}</p>
+                    </div>
+                </div>
+            `;
+        }
+    }
+
+    // Load the songs whenever a playlist is clicked
+    Array.from(document.getElementsByClassName("card")).forEach((e) => {
+        e.addEventListener("click", async (item) => {
+            songs = await fetchSongs(`Songs/${item.currentTarget.dataset.folder}`);
+        });
+    });
+}
+
+
+// Main Function
+async function main() {
+    // Fetch all songs
+    songs = await fetchSongs("Songs/Open_Letter");
+    playMusic(songs[0], true);
+
+    // Fetching all albums in playlists section
+    fetchAlbums();
 
     // Attach an event listener to play/pause, next and previous buttons
     const playButton =  document.querySelector("#play");
@@ -144,24 +193,24 @@ async function main() {
 
     // Attach event listener to previous button
     const previous = document.querySelector("#previous");
-    previous.addEventListener("click", () => {
+    previous.addEventListener("click", async () => {
         let currentSongSrc = currentSong.src.split("/").slice(-1)[0];
         let index = songs.indexOf(currentSongSrc);
         if(index-1 >= 0) {
+            songs = await fetchSongs(currentFolder);
             playMusic(songs[index-1]);
         }
     });
 
     // Attach event listener to next button
     const next = document.querySelector("#next");
-    next.addEventListener("click", () => {
+    next.addEventListener("click", async () => {
         let currentSongSrc = currentSong.src.split("/").slice(-1)[0];
         let index = songs.indexOf(currentSongSrc);
         if(index+1 < songs.length) {
+            songs = await fetchSongs(currentFolder);
             playMusic(songs[index+1]);
         }
     });
 }
 main();
-
-
